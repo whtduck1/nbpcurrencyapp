aaf242189629a0f354aa4306983c4f400f07fbfe
whtduck1 <myszormateusz02@gmail.com>
2026-06-05 16:52:44 +0200

    frontend done, connect with FastApi, solved CORS problem

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from database import get_db, engine
 import models
@@ -6,6 +7,18 @@ from nbp_service import fetch_rates_from_nbp
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="NBP Currency API")
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/currencies/fetch")
 def fetch_and_save_currencies(target_date: str, db: Session = Depends(get_db)):
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { CurrencyService } from './services/currency.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+title = 'NBP Currency App';
+  
+  currencies: any[] = [];
+  selectedDate: string = '2026-06-03'; 
+  message: string = '';
+  isError: boolean = false;
+
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit(): void {
+    this.loadAllCurrencies();
+  }
+
+  loadAllCurrencies(): void {
+    this.currencyService.getAvailableCurrencies().subscribe({
+      next: (data) => {
+        this.currencies = data;
+      },
+      error: (err) => {
+        this.message = 'Błąd podczas ładowania danych z bazy!';
+        this.isError = true;
+        console.error(err);
+      }
+    });
+  }
+
+  fetchFromNbp(): void {
+    if (!this.selectedDate) {
+      this.message = 'Wybierz poprawną datę!';
+      this.isError = true;
+      return;
+    }
+
+    this.message = 'Pobieranie danych z NBP...';
+    this.isError = false;
+
+    this.currencyService.fetchAndSaveCurrencies(this.selectedDate).subscribe({
+      next: (response) => {
+        this.message = `Sukces! Pobrano i zapisano rekordy z NBP dla daty: ${this.selectedDate}`;
+        this.isError = false;
+        this.filterByDate();
+      },
+      error: (err) => {
+        this.message = 'Błąd! NBP może nie mieć tabeli kursów dla tego dnia (np. weekend/święto) lub backend nie działa.';
+        this.isError = true;
+        console.error(err);
+      }
+    });
+  }
+
+  filterByDate(): void {
+    if (!this.selectedDate) return;
+
+    this.currencyService.getCurrenciesByDate(this.selectedDate).subscribe({
+      next: (data) => {
+        this.currencies = data;
+        if (data.length === 0) {
+          this.message = `Brak danych w bazie dla daty ${this.selectedDate}. Pobierz je najpierw z NBP!`;
+          this.isError = false;
+        }
+      },
+      error: (err) => {
+        this.message = 'Błąd podczas filtrowania danych!';
+        this.isError = true;
+        console.error(err);
+      }
+    });
+  }
 }
