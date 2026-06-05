6869b43ae7e4d0a9dd2f9b2ad10bb46cc50ce936
whtduck1 <myszormateusz02@gmail.com>
2026-06-05 15:46:03 +0200

    simple nbp and api endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+import models
+from nbp_service import fetch_rates_from_nbp
 
 app = FastAPI(title="NBP Currency API")
 
-@app.get("/")
-def read_root():
-    return {"status": "FastAPI works"}
+@app.post("/currencies/fetch")
+def fetch_and_save_currencies(target_date: str, db: Session = Depends(get_db)):
+    nbp_rates = fetch_rates_from_nbp(target_date)
+    
+    for rate in nbp_rates:
+        db_rate = models.CurrencyRate(
+            currency_code=rate["code"],
+            currency_name=rate["currency"],
+            exchange_rate=rate["mid"],
+            rate_date=target_date 
+        )
+        db.add(db_rate)
+        
+    db.commit()
+    return {"status": "Sukces", "pobrano": len(nbp_rates)}
+
+@app.get("/currencies")
+def get_available_currencies(db: Session = Depends(get_db)):
+    return db.query(models.CurrencyRate).all()
+
+@app.get("/currencies/{target_date}")
+def get_currencies_by_date(target_date: str, db: Session = Depends(get_db)):
+    return db.query(models.CurrencyRate).filter(models.CurrencyRate.rate_date == target_date).all()
